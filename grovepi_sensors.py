b3dac905c0882a72e68e3a72c743640edd670cbc
RakshetaK <15raksheta.k@gmail.com>
2025-10-20 19:40:32 -0700

    code

--- grovepi_sensors.py
+++ grovepi_sensors.py
@@ -1,6 +1,8 @@
-import sys
-sys.path.append('~/Dexter/GrovePi/Software/Python')
-import time
+import os,sys
+sys.path.append('~GrovePi/Software/Python')
+sys.path.append('~GrovePi/Software/Python/grove_rgb_lcd')
+
+
 import grovepi
 from grove_rgb_lcd import *
 
@@ -8,21 +10,32 @@ from grove_rgb_lcd import *
 ultrasonic_ranger = 2
 # potentiometer connected to analog port A0 as input
 potentiometer = 0
+
 grovepi.pinMode(potentiometer,"INPUT")
 
+
 # clear lcd screen  before starting main loop
-setText("")
+setText_norefresh("")
+adc_ref = 5
+grove_vcc= 5
+full_angle= 300
 
 while True:
   try:
-    # TODO:read distance value from Ultrasonic Ranger and print distance on LCD
-  
+    # TODO:read distance value from Ultrasonic Ranger and print distance on$
+    range = grovepi.ultrasonicRead(ultrasonic_ranger)
 
     # TODO: read threshold from potentiometer
+    sensor_value = grovepi.analogRead(potentiometer)
+    thresh_cm = int((sensor_value/1023.0)*200)
 
-    
     # TODO: format LCD text according to threshhold
-  
+    if(range <= thresh_cm):
+      setText_norefresh((str)(thresh_cm) + " OBJ PRES")
+    else:
+      setText_norefresh((str)(thresh_cm)+ " ")
+    
+    setText_norefresh("\n" + (str)(range))
     
   except IOError:
     print("Error")
